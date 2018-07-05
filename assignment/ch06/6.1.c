d827bc248cb8fbcaa91dcfdc4d3c01f2fd523ce6
paradoxnafi <paradoxnafi@gmail.com>
2018-07-05 08:39:19 -0700

    Everything is working just fine

--- assignment/ch06/6.1.c
+++ assignment/ch06/6.1.c
@@ -1,13 +1,14 @@
+// Get an input number from user and print it in reverse order.
+
 #include <stdio.h>
 
 int main()
 {
-	int n, store[100], i, num, count, backup;
+	int n, store[100], i, num, count;
 	count = 0;
 
 	printf("Enter any number: ");
 	scanf(" %d", &num);
-	backup = num;
 
 	for(i = 0; num > 0; i++) {
 		store[i] = num % 10;
@@ -15,8 +16,10 @@ int main()
 		count++;
 	}
 
-	for(i = 0; i < count; i++) {
+	i = 0;
+	while(i < count) {
 		printf("%d", store[i]);
+		i++;
 	}
 
 	printf("\n");
