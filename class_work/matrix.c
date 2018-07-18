459914de77093e934bb6ba41ad64d246d7f60698
Md. Nafi shahriyar <32407364+paradoxnafi@users.noreply.github.com>
2018-07-18 11:28:38 +0600

    It is now interective.

--- class_work/matrix.c
+++ class_work/matrix.c
@@ -2,13 +2,18 @@
 
 int main()
 {
-	int a[2][2];
-	int b[2][2];
-	int c[2][2];
+	int m, n;
+
+	printf("Enter dimention of array: ");
+	scanf("%d %d", &m, &n);
+
+	int a[m][n];
+	int b[m][n];
+	int c[m][m];
 	int i, j;
 
-	for(i = 0; i <= 1; i++) {
-		for(j = 0; j <= 1; j++) {
+	for(i = 0; i <= m-1; i++) {
+		for(j = 0; j <= n-1; j++) {
 			printf("a[%d][%d]: ", i+1, j+1);
 			scanf("%d", &a[i][j]);
 		}
@@ -16,23 +21,23 @@ int main()
 
 	printf("\n");
 
-	for(i = 0; i <= 1; i++) {
-		for(j = 0; j <= 1; j++) {
+	for(i = 0; i <= m-1; i++) {
+		for(j = 0; j <= n-1; j++) {
 			printf("b[%d][%d]: ", i+1, j+1);
 			scanf("%d", &b[i][j]);
 		}
 	}
 
-	for(i = 0; i <= 1; i++) {
-		for(j = 0; j <= 1; j++) {
+	for(i = 0; i <= m-1; i++) {
+		for(j = 0; j <= m-1; j++) {
 			c[i][j] = a[i][j] + b[i][j];
 		}
 	}
 
 	printf("\n");
 	
-	for(i = 0; i <= 1; i++) {
-		for(j = 0; j <= 1; j++) {
+	for(i = 0; i <= m-1; i++) {
+		for(j = 0; j <= n-1; j++) {
 			printf("%d ", c[i][j]);
 		}
 
